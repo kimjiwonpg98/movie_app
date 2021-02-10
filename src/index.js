be4ec6a8e85ab1c8aa0aa0d996c21fbf6a0268e7
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-02-11 01:32:25 +0900

    add .env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Potato from "./potato";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Potato />
   </React.StrictMode>,
   document.getElementById("root")
 );

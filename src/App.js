ced41b5f3ac38e245b332d10d8ce47f9dee6a3bc
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-02-12 00:19:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Helllo</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => this.setState((current) => ({ count: current.count + 1 }));
+  minus = () => this.setState((current) => ({ count: current.count - 1 }));
+
+  render() {
+    return (
+      <div>
+        <h1>count is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
